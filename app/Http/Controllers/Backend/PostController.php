6199f7ebf8c30f9c256a1b2d64bbd687d9357f3a
mohsin <mohsin.sigmato@gmail.com>
2017-09-20 11:43:59 +0530

    Add All files

---
+++ app/Http/Controllers/Backend/PostController.php
@@ -0,0 +1,249 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use App\Models\Post;
+use App\Repositories\Contracts\PostRepository;
+use App\Repositories\Contracts\TagRepository;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Yajra\DataTables\Facades\DataTables;
+
+class PostController extends BackendController
+{
+    /**
+     * @var PostRepository
+     */
+    protected $posts;
+
+    /**
+     * @var TagRepository
+     */
+    protected $tags;
+
+    /**
+     * Create a new controller instance.
+     *
+     *
+     * @param \App\Repositories\Contracts\PostRepository $posts
+     * @param \App\Repositories\Contracts\TagRepository  $tags
+     */
+    public function __construct(PostRepository $posts, TagRepository $tags)
+    {
+        $this->posts = $posts;
+        $this->tags = $tags;
+    }
+
+    public function getDraftPostCounter()
+    {
+        return $this->posts->query()->whereStatus(Post::DRAFT)->count();
+    }
+
+    public function getPendingPostCounter()
+    {
+        return $this->posts->query()->whereStatus(Post::PENDING)->count();
+    }
+
+    public function getPublishedPostCounter()
+    {
+        return $this->posts->query()->whereStatus(Post::PUBLISHED)->count();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function getLastestPosts()
+    {
+        $query = $this->posts->query();
+
+        if (!Gate::check('view posts')) {
+            // Filter to only current user's posts
+            $query->whereUserId(auth()->id());
+        }
+
+        return $query->orderByDesc('created_at')->limit(10)->get();
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @param Request $request
+     *
+     * @throws \Exception
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function search(Request $request)
+    {
+        if ($request->isXmlHttpRequest()) {
+            /** @var Builder $query */
+            $query = $this->posts->select([
+                'id',
+                'user_id',
+                'status',
+                'pinned',
+                'promoted',
+                'created_at',
+                'updated_at',
+            ]);
+
+            if (!Gate::check('view posts')) {
+                // Filter to only current user's posts
+                $query->whereUserId(auth()->id());
+            }
+
+            /** @var \Yajra\DataTables\EloquentDataTable $query */
+            $query = DataTables::of($query);
+
+            return $query->addColumn('actions', function (Post $post) {
+                return $this->posts->getActionButtons($post);
+            })->addColumn('image', function (Post $post) {
+                $content = image_template_html('small', $post->featured_image_path, $post->title);
+
+                return "<a href=\"#/posts/{$post->id}/edit\">{$content}</a>";
+            })->editColumn('title', function (Post $post) {
+                return "<a href=\"#/posts/{$post->id}/edit\">{$post->title}</a>";
+            })->editColumn('status', function (Post $post) {
+                return state_html_label($post->state, trans($post->status_label));
+            })->editColumn('pinned', function (Post $post) {
+                return boolean_html_label($post->pinned);
+            })->editColumn('promoted', function (Post $post) {
+                return boolean_html_label($post->promoted);
+            })->editColumn('created_at', function (Post $post) use ($request) {
+                return $post->created_at->setTimezone($request->user()->timezone);
+            })->editColumn('updated_at', function (Post $post) use ($request) {
+                return $post->updated_at->setTimezone($request->user()->timezone);
+            })
+                ->rawColumns(['image', 'title', 'status', 'pinned', 'promoted', 'actions'])
+                ->make(true);
+        }
+    }
+
+    /**
+     * @param Post $post
+     *
+     * @return Post
+     */
+    public function show(Post $post)
+    {
+        $this->authorize('view', $post);
+
+        $post->tags = $post->tags()->pluck('name');
+
+        return $post;
+    }
+
+    /**
+     * @param StorePostRequest $request
+     *
+     * @return mixed
+     */
+    public function store(StorePostRequest $request)
+    {
+        $this->authorize('create posts');
+
+        /** @var Post $post */
+        $post = $this->posts->make(
+            $request->only('title', 'summary', 'body', 'published_at', 'unpublished_at', 'pinned', 'promoted')
+        );
+
+        if ('publish' === $request->input('status')) {
+            $this->posts->saveAndPublish($post, $request->input(), $request->file('featured_image'));
+        } else {
+            $this->posts->saveAsDraft($post, $request->input(), $request->file('featured_image'));
+        }
+
+        return $this->RedirectResponse($request, trans('alerts.backend.posts.created'));
+    }
+
+    /**
+     * @param Post              $post
+     * @param UpdatePostRequest $request
+     *
+     * @throws \Illuminate\Database\Eloquent\MassAssignmentException
+     *
+     * @return mixed
+     */
+    public function update(Post $post, UpdatePostRequest $request)
+    {
+        $this->authorize('update', $post);
+
+        $post->fill(
+            $request->only('title', 'summary', 'body', 'published_at', 'unpublished_at', 'pinned', 'promoted')
+        );
+
+        if ('publish' === $request->input('status')) {
+            $this->posts->saveAndPublish($post, $request->input(), $request->file('featured_image'));
+        } else {
+            $this->posts->saveAsDraft($post, $request->input(), $request->file('featured_image'));
+        }
+
+        return $this->RedirectResponse($request, trans('alerts.backend.posts.updated'));
+    }
+
+    /**
+     * @param Post    $post
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public function destroy(Post $post, Request $request)
+    {
+        $this->authorize('delete', $post);
+
+        $this->posts->destroy($post);
+
+        return $this->RedirectResponse($request, trans('alerts.backend.posts.deleted'));
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array|\Illuminate\Http\RedirectResponse
+     */
+    public function batchAction(Request $request)
+    {
+        $action = $request->get('action');
+        $ids = $request->get('ids');
+
+        switch ($action) {
+            case 'destroy':
+                $this->authorize('delete posts');
+
+                $this->posts->batchDestroy($ids);
+
+                return $this->RedirectResponse($request, trans('alerts.backend.posts.bulk_destroyed'));
+                break;
+            case 'publish':
+                $this->authorize('edit posts');
+
+                $this->posts->batchPublish($ids);
+
+                if (Gate::check('publish posts')) {
+                    return $this->RedirectResponse($request, trans('alerts.backend.posts.bulk_published'));
+                }
+
+                return $this->RedirectResponse($request, trans('alerts.backend.posts.bulk_pending', 'warning'));
+                break;
+            case 'pin':
+                $this->authorize('edit posts');
+
+                $this->posts->batchPin($ids);
+
+                return $this->RedirectResponse($request, trans('alerts.backend.posts.bulk_pinned'));
+                break;
+            case 'promote':
+                $this->authorize('edit posts');
+
+                $this->posts->batchPromote($ids);
+
+                return $this->RedirectResponse($request, trans('alerts.backend.posts.bulk_promoted'));
+                break;
+        }
+
+        return $this->RedirectResponse($request, trans('alerts.backend.actions.invalid'), 'error');
+    }
+}
---
+++ app/Models/Access/Post.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\Metable;
+use App\Models\Traits\Taggable;
+use App\Models\Traits\Translatable;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Plank\Mediable\Media;
+use Plank\Mediable\Mediable;
+
+/**
+ * App\Models\Post.
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property int $status
+ * @property bool $promoted
+ * @property bool $pinned
+ * @property \Carbon\Carbon|null $published_at
+ * @property string|null $unpublished_at
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property mixed $featured_image_path
+ * @property mixed $featured_image_url
+ * @property mixed $meta_description
+ * @property mixed $meta_title
+ * @property mixed $published
+ * @property mixed $state
+ * @property mixed $status_label
+ * @property \Illuminate\Database\Eloquent\Collection|\Plank\Mediable\Media[] $media
+ * @property \App\Models\Meta $meta
+ * @property \App\Models\User|null $owner
+ * @property \Illuminate\Database\Eloquent\Collection|\App\Models\Tag[] $tags
+ * @property \Illuminate\Database\Eloquent\Collection|\App\Models\PostTranslation[] $translations
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post listsTranslations($translationField)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post notTranslatedIn($locale = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post orWhereTranslation($key, $value, $locale = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post orWhereTranslationLike($key, $value, $locale = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post published()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post translated()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post translatedIn($locale = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereHasMedia($tags, $match_all = false)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereHasMediaMatchAll($tags)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post wherePinned($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post wherePromoted($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post wherePublishedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereTranslation($key, $value, $locale = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereTranslationLike($key, $value, $locale = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereUnpublishedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post withMedia($tags = array(), $match_all = false)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post withMediaMatchAll($tags = array())
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post withOwner(\App\Models\User $user)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post withTag(\App\Models\Tag $tag)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post withTranslation()
+ * @mixin \Eloquent
+ */
+class Post extends Model
+{
+    use Mediable;
+    use Taggable;
+    use Metable;
+    use Translatable;
+
+    public $translatedAttributes = ['title', 'summary', 'body', 'slug'];
+
+    protected $dates = [
+        'published_at',
+        'unpublished_at',
+    ];
+
+    protected $appends = [
+        'state', 'status_label', 'featured_image_path',
+    ];
+
+    protected $casts = [
+        'pinned' => 'boolean',
+        'promoted' => 'boolean',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['status', 'published_at', 'unpublished_at', 'pinned', 'promoted'];
+
+    protected $with = ['translations', 'media', 'owner'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleted(function (Post $post) {
+            $post->meta->delete();
+        });
+    }
+
+    const DRAFT = 0;
+    const PENDING = 1;
+    const PUBLISHED = 2;
+
+    public static function getStatuses()
+    {
+        return [
+            self::DRAFT => 'labels.backend.posts.statuses.draft',
+            self::PENDING => 'labels.backend.posts.statuses.pending',
+            self::PUBLISHED => 'labels.backend.posts.statuses.published',
+        ];
+    }
+
+    public static function getStates()
+    {
+        return [
+            self::DRAFT => 'danger',
+            self::PENDING => 'warning',
+            self::PUBLISHED => 'success',
+        ];
+    }
+
+    public function getStatusLabelAttribute()
+    {
+        return self::getStatuses()[$this->status];
+    }
+
+    public function getStateAttribute()
+    {
+        return self::getStates()[$this->status];
+    }
+
+    public function getPublishedAttribute()
+    {
+        return self::PUBLISHED === $this->status;
+    }
+
+    public function getFeaturedImagePathAttribute()
+    {
+        /** @var Media $media */
+        if ($media = $this->getMedia('featured image')->first()) {
+            return $media->getDiskPath();
+        }
+
+        return 'placeholder.png';
+    }
+
+    public function getFeaturedImageUrlAttribute()
+    {
+        return config('filesystems.disks.public.url').'/'.$this->getFeaturedImagePathAttribute();
+    }
+
+    public function getMetaTitleAttribute()
+    {
+        return null !== $this->meta && !empty($this->meta->title) ? $this->meta->title : $this->title;
+    }
+
+    public function getMetaDescriptionAttribute()
+    {
+        return null !== $this->meta && !empty($this->meta->description) ? $this->meta->description : $this->summary;
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function setPublishedAtAttribute($value)
+    {
+        if (is_string($value)) {
+            $this->attributes['published_at'] = Carbon::createFromFormat('Y-m-d H:i', $value);
+        } else {
+            $this->attributes['published_at'] = $value;
+        }
+    }
+
+    public function setUnPublishedAtAttribute($value)
+    {
+        if (is_string($value)) {
+            $this->attributes['unpublished_at'] = Carbon::createFromFormat('Y-m-d H:i', $value);
+        } else {
+            $this->attributes['unpublished_at'] = $value;
+        }
+    }
+
+    /**
+     * Scope a query to only include published articles.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopePublished(Builder $query)
+    {
+        return $query->where('status', '=', self::PUBLISHED);
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param \App\Models\User                      $user
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeWithOwner(Builder $query, User $user)
+    {
+        return $query->where('user_id', '=', $user->id);
+    }
+
+    /**
+     * Delete media physically.
+     *
+     * @throws \Exception
+     */
+    public function handleMediableDeletion()
+    {
+        $this->getMedia('featured image')->each(function (Media $media) {
+            $media->delete();
+        });
+    }
+}
